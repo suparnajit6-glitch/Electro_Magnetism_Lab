eff88210f74e8b400db6b235ee884993163a1ca3
suparnajit6-glitch <suparnajit6@gmail.com>
2026-04-08 02:28:21 +0530

    Some refinement

--- 3D_ydse.py
+++ 3D_ydse.py
@@ -6,12 +6,15 @@ from matplotlib import colors
 from mpl_toolkits.mplot3d import axes3d
 
 
-plt.style.use('dark_background')
+
+#Data input from user:
 
 
 
 S=float(input("whats the screen pos? :"))
 t=float(input("t at which to present the snapshot? :"))
+
+#Initialize:
 
 x=np.linspace(0,S,1000)
 y=np.linspace(-100,100,1000)
@@ -22,19 +25,23 @@ X,Y=np.meshgrid(x,y)
 
 Γ,ζ=np.meshgrid(y,z)
 
+#Constants :
 
-λ=5*(1e-6)
-v=3*(1e8) 
+λ=700*(1e-9)
+c=3*(1e8) 
 A=1
-k=np.pi/λ
-ω=v*k
+k=2*np.pi/λ
+ω=c*k
 
-#Sources
+ 
 
-s1=(0,0.0001,1)
-s2=(0,-0.0001,1)
 
-# Defining the EM waves
+#Sources  (At the level of μm ):
+
+s1=(0,0.000001,1)
+s2=(0,-0.000001,1)
+
+# Defining the light waves:
 
 def o1(x,y,z,t):
 
@@ -52,17 +59,17 @@ def get_wave():
     wave = np.where(X<=S,((o1(X,Y,1,t) + o2(X,Y,1,t))**2),0)
     return wave
 
-#inst
+#Intensity (instantanous) calculation:
 def get_int_screen(t):
     I=(o1(S,Γ,ζ,t)+o2(S,Γ,ζ,t))**2
 
     return I
 
 
+#Plotting:
 
-Z = get_wave() # keep the surface height between -1 and 1
 
-
+Z = get_wave() 
 
 fig = plt.figure(figsize=(14, 8))
 gs = fig.add_gridspec(3, 2)
@@ -71,8 +78,10 @@ ax1 = fig.add_subplot(gs[:, 0], projection='3d')  # large 3D
 ax1.plot_surface(X,Y,Z,cmap="RdGy",linewidth=0)
 
 ax2 = fig.add_subplot(gs[0, 1])
+ax2.set_xlim(-10,10)
+ax2.set_ylim(-10,10)
 ax2.contourf(Γ,ζ,get_int_screen(t),levels=100,cmap="magma")
-ax2.grid(True,color="white",linestyle="--")
+ax2.grid(True,color="gray",linestyle="--",linewidth=0.5)
 
 ax3 = fig.add_subplot(gs[1, 1])
 ax4 = fig.add_subplot(gs[2, 1])
