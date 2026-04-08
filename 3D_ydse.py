a1a783053bbff639cc79abd9a6e3eedf058d0257
suparnajit6-glitch <suparnajit6@gmail.com>
2026-04-09 02:46:53 +0530

    Refine YDSE 3D notebook plot layout and labels

--- 3D_ydse.py
+++ 3D_ydse.py
@@ -16,9 +16,9 @@ t=float(input("t at which to present the snapshot? :"))
 
 #Initialize:
 
-x=np.linspace(0,S,1000)
-y=np.linspace(-100,100,1000)
-z=np.linspace(-100,100,1000)
+x=np.linspace(0,S,300)
+y=np.linspace(-100,100,200)
+z=np.linspace(-100,100,200)
 
 
 X,Y=np.meshgrid(x,y)
@@ -27,32 +27,31 @@ X,Y=np.meshgrid(x,y)
 
 #Constants :
 
-λ=700*(1e-9)
-c=3*(1e8) 
+λ=10
+c=3 
 A=1
 k=2*np.pi/λ
 ω=c*k
 
  
 
-
 #Sources  (At the level of μm ):
 
-s1=(0,0.000001,1)
-s2=(0,-0.000001,1)
+s1=(0,20,1)
+s2=(0,-20,1)
 
 # Defining the light waves:
 
 def o1(x,y,z,t):
 
     r1 = np.sqrt((x-s1[0])**2 + (y-s1[1])**2 +(z-s1[2])**2 )
-    a=A*np.sin(ω*t-k*r1)/(np.sqrt(r1))
+    a=A*np.sin(ω*t-k*r1)
     return a
 
 def o2(x,y,z,t):
 
     r2 = np.sqrt((x-s2[0])**2 + (y-s2[1])**2 + (z-s2[2])**2)
-    a= A*np.sin(ω*t-k*r2)/(np.sqrt(r2))
+    a= A*np.sin(ω*t-k*r2)
     return a 
 
 def get_wave():
@@ -75,11 +74,15 @@ fig = plt.figure(figsize=(14, 8))
 gs = fig.add_gridspec(3, 2)
 
 ax1 = fig.add_subplot(gs[:, 0], projection='3d')  # large 3D
-ax1.plot_surface(X,Y,Z,cmap="RdGy",linewidth=0)
+ax1.plot_surface(X,Y,Z,cmap="RdGy",linewidth=1,antialiased=False,rcount=400,ccount=400)
+ax1.set_xlim3d(0,S)
+ax1.set_ylim3d(-100,100)
+ax1.set_zlim3d(-10,20)
+
 
 ax2 = fig.add_subplot(gs[0, 1])
-ax2.set_xlim(-10,10)
-ax2.set_ylim(-10,10)
+ax2.set_xlim(min(y),max(y))
+ax2.set_ylim(min(z),max(z))
 ax2.contourf(Γ,ζ,get_int_screen(t),levels=100,cmap="magma")
 ax2.grid(True,color="gray",linestyle="--",linewidth=0.5)
 
